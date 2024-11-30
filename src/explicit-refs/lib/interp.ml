acfd8baac1527c565b8ae8a774d032c642e21b25
Ricky172Patel <patel.ricky172@gmail.com>
2024-11-30 02:35:22 -0500

    Homework 4: Added records with mutable fields

--- src/explicit-refs/lib/interp.ml
+++ src/explicit-refs/lib/interp.ml
@@ -1,8 +1,21 @@
+(* 
+  Name: Ricky Patel
+  CWID: 20032062
+  RPATEL29
+  CS510A
+*)
+
 open Ds
 open Parser_plaf.Ast
 open Parser_plaf.Parser
     
 let g_store = Store.empty_store 20 (NumVal 0)
+
+let rec addIds fs evs =
+  match fs,evs with
+  | [], [] -> []
+  | (id,(is_mutable,_)) :: fs', v :: evs' -> (id,(is_mutable,v)) :: addIds fs' evs'
+  | _,_ -> failwith "addIds: mismatched lists"
 
 let rec eval_expr : expr -> exp_val ea_result = fun e ->
   match e with
@@ -92,12 +105,70 @@ let rec eval_expr : expr -> exp_val ea_result = fun e ->
     sequence (List.map eval_expr es) >>= fun l ->
     return (List.hd (List.rev l))
   | Unit -> return UnitVal
+  | IsEqual(e1,e2) ->
+    eval_expr e1 >>= 
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (BoolVal (n1 = n2))
+  | IsGT(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (BoolVal (n1 > n2))
+  | IsLT(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (BoolVal (n1 < n2))
+  | IsNumber(e) ->
+    eval_expr e >>= fun n ->
+      (match n with
+      | NumVal _ -> return (BoolVal true)
+      | _ -> return (BoolVal false))
+  | Record(fs ) ->
+    sequence (List.map process_field fs) >>= fun evs ->
+    return (RecordVal (addIds fs evs))
+  | Proj(e,id) ->
+    eval_expr e >>= 
+    fields_of_recordVal >>= fun ev ->
+      (match (List.assoc_opt id ev) with
+      | Some v -> project_field (id,v)
+      | None -> error "The field is not present in the record" ) 
+  | SetField(e1,id,e2) ->
+    eval_expr e1 >>=
+    fields_of_recordVal >>= fun evs ->
+      (match (List.assoc_opt id evs) with
+      | Some v -> field_setter (id,v) e2
+      | None -> error "The field is not present in the record" )
   | Debug(_e) ->
     string_of_env >>= fun str_env ->
     let str_store = Store.string_of_store string_of_expval g_store 
     in (print_endline (str_env^"\n"^str_store);
     error "Reached breakpoint")
   | _ -> failwith ("Not implemented: "^string_of_expr e)
+  and
+  process_field (_id,(is_mutable,e)) =
+  eval_expr e >>= fun ev ->
+  if is_mutable
+  then return (RefVal (Store.new_ref g_store ev))
+  else return ev
+  and
+  project_field (_id,(is_mutable,e)) =
+    if is_mutable
+      then int_of_refVal e >>= (Store.deref g_store)
+    else return e
+  and
+  field_setter (_id,(is_mutable,e)) e2 =
+  if is_mutable
+    then int_of_refVal e >>= fun v ->
+    eval_expr e2 >>= fun ev -> 
+    Store.set_ref g_store v ev >>= fun _ ->
+    return (UnitVal)
+  else error "Cannot set a non-mutable field"
+
 
 let eval_prog (AProg(_,e)) =
   eval_expr e         
@@ -107,5 +178,10 @@ let interp (s:string) : exp_val result =
   let c = s |> parse |> eval_prog
   in run c
 
-
+(* Interpret an expression read from a file with optional extension .exr *)
+let interpf (s:string) : exp_val result =
+  let s = String.trim s       (* remove leading and trailing spaces *)
+  in let file_name =          (* allow rec to be optional *)
+    match String.index_opt s '.' with None -> s^".exr" | _ -> s
+  in interp @@ read_file file_name
 
