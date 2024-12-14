367a685b28bb7af3697fc1d65f2c394adcb585bb
Ricky172Patel <patel.ricky172@gmail.com>
2024-12-14 01:35:48 -0500

    HW6: Extension to SOOL

--- src/sool/lib/interp.ml
+++ src/sool/lib/interp.ml
@@ -1,3 +1,10 @@
+(* 
+  Name: Ricky Patel
+  CWID: 20032062
+  RPATEL29
+  CS510A
+*)
+
 open Ds
 open ReM
 open Parser_plaf.Ast
@@ -18,7 +25,17 @@ let g_store = Store.empty_store 20 (NumVal 0)
 (* Global holding class declarations *)
 let g_class_env : class_env ref = ref []
 
+let rec is_subclass : string -> string -> class_env -> exp_val ea_result =
+  fun c_name1 c_name2 c_env ->
+    (match List.assoc_opt c_name2 c_env with
+    | None -> error ("is_subclass: class " ^ c_name2 ^ " not found")
+    | Some _ -> 
+      (match List.assoc_opt c_name1 c_env with 
+      | None -> return (BoolVal false)
+      | Some (super,_,_) -> if c_name1 = c_name2 then return (BoolVal true) else (is_subclass super c_name2 c_env)))
 
+let name_mangle n es =
+  n^"_"^string_of_int (List.length es)
 
 (* Initialize contents of g_class_env variable  *)
 
@@ -42,7 +59,7 @@ let initialize_class_env cs =
     | [] -> []
     | Class (name,super,_impl,_fields,methods)::_  when name=c_name ->
       (List.map (fun (Method(n,_ret_type,pars,body))
-                  -> (n,(List.map fst pars,body,super,List.flatten fss)))
+                  -> ((name_mangle n pars),(List.map fst pars,body,super,List.flatten fss)))
          methods) @ get_methods cs super (List.tl fss) cs
     | Class (_,_,_,_,_)::cs'  | Interface(_,_)::cs'
       -> get_methods cs c_name fss cs'
@@ -53,7 +70,7 @@ let initialize_class_env cs =
       | Class (name,super,_impl,fields,methods)::cs'  ->
         let fss = (List.map fst fields) :: get_fields cs super cs
         in let ms = (List.map (fun (Method(n,_ret_type,pars,body))
-                                -> (n,(List.map fst pars,body,super,List.flatten fss)))
+                                -> ((name_mangle n pars),(List.map fst pars,body,super,List.flatten fss)))
                        methods) @ get_methods cs super (List.tl fss) cs
         in
         g_class_env := (name,(super,List.flatten fss,ms))::!g_class_env;
@@ -213,17 +230,17 @@ and
      | Some (_super,fields,methods) -> 
        new_env fields >>= fun env ->
        let self = ObjectVal(c_name,env)
-       in (match List.assoc_opt "initialize" methods with
+       in (match List.assoc_opt (name_mangle "initialize" args) methods with
            | None -> return self
-           | Some m -> apply_method "initialize" self args m >>= fun _ ->
+           | Some m -> apply_method (name_mangle "initialize" args) self args m >>= fun _ ->
              return self))
   | Send(e,m_name,es) ->
     eval_expr e >>= fun self ->
     obj_of_objectVal self >>= fun (c_name,_) ->
     eval_exprs es >>= fun args ->
-    (match lookup_method c_name m_name !g_class_env with
+    (match lookup_method c_name (name_mangle m_name args) !g_class_env with
      | None -> error "Method not found"
-     | Some m -> apply_method m_name self args m)
+     | Some m -> apply_method (name_mangle m_name args) self args m)
   | Self ->
     eval_expr (Var "_self")
   | Super(m_name,es) ->
@@ -231,9 +248,9 @@ and
     eval_expr (Var "_super") >>=
     string_of_stringVal >>= fun c_name ->
     eval_expr (Var "_self") >>= fun self ->
-    (match lookup_method c_name m_name !g_class_env with
+    (match lookup_method c_name (name_mangle m_name args) !g_class_env with
      | None -> error "Method not found"
-     | Some m -> apply_method m_name self args m)
+     | Some m -> apply_method (name_mangle m_name args) self args m)
   (* List operations* *)
   | List(es) ->
     eval_exprs es >>= fun args ->
@@ -255,6 +272,11 @@ and
     eval_expr e >>=
     list_of_listVal >>= fun l ->
     return @@ BoolVal (l=[])   
+  | IsInstanceOf(e,id) -> 
+    eval_expr e >>= fun ev ->
+    (match ev with
+    | ObjectVal(c_name, _) -> (is_subclass c_name id !g_class_env)
+    | _ -> error "IsInstanceOf: Expected ObjectVal")
   (* Debug *)
   | Debug(_e) ->
     string_of_env >>= fun str_env ->
